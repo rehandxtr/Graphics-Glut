4b27c024e138ee8c42791f8f7c29f31df944576f
rehandxtr <38095118+rehandxtr@users.noreply.github.com>
2019-06-26 14:14:43 +0530

    Add files via upload

---
+++ Analog-clock.c
@@ -0,0 +1,139 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <math.h>
+#include <time.h>
+#include <sys/timeb.h>
+
+
+
+const float clockR    = 50.0f,
+            clockVol  = 100.0f,
+
+            angle1min = M_PI / 30.0f,
+
+            minStart  = 0.9f,
+            minEnd    = 1.0f,
+
+            stepStart = 0.8f,
+            stepEnd   = 1.0f;
+
+float angleHour = 0,
+      angleMin  = 0,
+      angleSec  = 0;
+
+
+
+void newLine(float rStart, float rEnd, float angle){
+  float c = cos(angle), s = sin(angle);
+  glVertex2f( clockR*rStart*c, clockR*rStart*s);
+  glVertex2f( clockR*rEnd*c, clockR*rEnd*s);
+}
+
+
+
+void RenderScene(void){
+  int i;
+
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+  glLineWidth(2.0f);
+  glEnable(GL_LINE_SMOOTH);
+  glEnable(GL_POINT_SMOOTH);
+  glEnable(GL_POLYGON_SMOOTH);
+
+  glBegin(GL_LINES);
+    for(i=0; i<60; i++){
+      if(i%5){ // normal minute
+        if(i%5 == 1)
+          glColor4f(0.0f, 1.0f, 1.0f, 1.0f);//light blue
+        newLine(minStart, minEnd, i*angle1min);
+      }else{
+        glColor4f(1.0f, 1.0f, 0.0f, 0.0f);//yellow
+        newLine(stepStart, stepEnd, i*angle1min);
+      }
+    }
+  glEnd();
+
+  glLineWidth(3.0f);
+  glBegin(GL_LINES);
+    newLine(0.0f, 0.5f, -angleHour+M_PI/2);
+    newLine(0.0f, 0.8f, -angleMin+M_PI/2);
+  glEnd();
+
+  glLineWidth(1.0f);
+  glColor3f(0.0f, 1.0f, 0.0f);//Green
+  glBegin(GL_LINES);
+    newLine(0.0f, 0.8f, -angleSec+M_PI/2);
+  glEnd();
+  
+  	glTranslatef(0.1,0,0);
+  	glTranslatef(0,0.1,0);
+
+  glutSwapBuffers();
+}
+
+
+
+///////////////////////////////////////////////////////////
+// Called by GLUT library when the window has chanaged size
+void ChangeSize(GLsizei w, GLsizei h){
+  GLfloat aspectRatio;
+
+  // Prevent a divide by zero
+  if(h == 0)
+    h = 1;
+
+  // Set Viewport to window dimensions
+  glViewport(0, 0, w, h);
+
+  // Reset coordinate system
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  // Establish clipping volume (left, right, bottom, top, near, far)
+  aspectRatio = (GLfloat)w / (GLfloat)h;
+  if (w <= h)
+    glOrtho (-clockVol, clockVol, -clockVol / aspectRatio, clockVol / aspectRatio, 1.0, -1.0);
+  else
+    glOrtho (-clockVol * aspectRatio, clockVol * aspectRatio, -clockVol, clockVol, 1.0, -1.0);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  glTranslatef(0.1,0,0);
+  glTranslatef(0,0.1,0);
+  	
+}
+
+
+void TimerFunction(int value){
+  struct timeb tb;
+  time_t tim=time(0);
+  struct tm* t;    
+  t=localtime(&tim);
+  ftime(&tb);
+
+  angleSec = (float)(t->tm_sec+ (float)tb.millitm/1000.0f)/30.0f * M_PI;
+  angleMin = (float)(t->tm_min)/30.0f * M_PI + angleSec/60.0f;
+  angleHour = (float)(t->tm_hour > 12 ? t->tm_hour-12 : t->tm_hour)/6.0f * M_PI+
+              angleMin/12.0f;
+
+  glutPostRedisplay();
+  glutTimerFunc(33,TimerFunction, 1);
+}
+
+///////////////////////////////////////////////////////////
+// Main program entry point
+int main(int argc, char* argv[]){
+  glutInit(&argc, argv);
+  glutCreateWindow("glClock by Roll No. 150 & 152");
+
+  glutDisplayFunc(RenderScene);
+  glutReshapeFunc(ChangeSize);
+  glutTimerFunc(33, TimerFunction, 1);
+  glutMainLoop();
+  
+  
+  return 0;
+}
